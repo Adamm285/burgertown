921407dada3c52961ca2f340bda1acfd50f8a54a
Adam m <adam@floorsmtihllc.com>
2020-01-21 17:54:33 -0500

    sever setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (name) VALUES ('Bobbi');
+INSERT INTO burgers (name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, devoured) VALUES ('Marilyn Manson', true);
+
